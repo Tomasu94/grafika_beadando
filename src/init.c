cb79e8e050c59ff644491e2ff4398554acc15b81
Tomasu94 <32292123+Tomasu94@users.noreply.github.com>
2020-05-14 18:58:59 +0200

    Update init.c

--- src/init.c
+++ src/init.c
@@ -9,6 +9,20 @@ Move move;
 int help;
 
 void init(){
+	
+    GLfloat light_position[] = {200.0f, 300.0f, 100.0f, 0.0f};
+    GLfloat light_ambient[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+    GLfloat light_diffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+	
 	set_callbacks();
 	init_camera(&camera);
 	init_entities(&world, move);
